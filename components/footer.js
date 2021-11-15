954742efcb673fdda238cde49f189354953a722a
dimasna <dimasna96@gmail.com>
2021-11-15 23:40:28 +0700

    title

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
 export default function Footer(){
     return (
-        <div>
+        <div className="absolute bottom-0">
             Footer
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,11 @@ export default function Home() {
         </title>
       </Head>
       <div className="flex h-32 bg-gradient-to-b from-gray-100"></div>
-      <section>
-        
+      <section className="lg:px-44 md:px-12 -mt-8">
+        <div className="flex items-baseline">
+        <p className="text-8xl">👨‍💻</p>
+        <p className="text-4xl font-bold">Hi, I'm Dimas N Al<span >👋</span> </p>
+        </div>
       </section>
     </Layout>
   )
